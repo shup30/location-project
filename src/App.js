ac2fa6cc8b02a3ccceb14b5c30050e595fd51ad1
Shubham <shup30@gmail.com>
2019-12-06 23:23:20 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React from "react";
-import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <br></br>
-        </div>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: null,
+      errorMessage: ""
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        //we called setState
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div> Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div> latitude: {this.state.lat}</div>;
+    }
+
+    return <div> Loading </div>;
+  }
 }
 
 export default App;
