ed9af686d16669974c1c9272dc835ae65f0ed67d
Shubham <shup30@gmail.com>
2019-12-12 16:03:54 +0530

    new Commit

--- Pics/src/SearchBar.js
+++ Pics/src/SearchBar.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
 
-const SearchBar = () => {
+class SearchBar extends React.Component {
+  state = { term: '' };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state.term);
+  };
+
+  render() {
     return (
-        <div>
-            Search Bar
-        </div>
-    )
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default SearchBar
+export default SearchBar;
